96972459945db5374db2b0da6b84795f419342dd
Charann05 <charannpoojary@gmail.com>
2025-09-28 17:58:56 +0530

    Test files

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import cv2
-import os
+#import os
 
-file_path = r'C:\VS Code\retinopathy_project\data\images\0a4e1a29ffff.png'
+file_path = r'data\images\0a4e1a29ffff.png'
 
-if not os.path.exists(file_path):
-    raise FileNotFoundError(f"File not found: {file_path}")
+#if not os.path.exists(file_path):
+    #raise FileNotFoundError(f"File not found: {file_path}")
 
 img = cv2.imread(file_path)
 
