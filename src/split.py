d06ca59dd9fedd0dbd5c1023733ca3518187995b
Charann05 <charannpoojary@gmail.com>
2025-10-03 18:20:12 +0530

    Divided the CSV for training and testing purposes

---
+++ src/split.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv(r"C:\VS Code\retinopathy_project\data\labels.csv", skiprows = 1, names=["code_id","diagnosis"])
+print(df.head)
+print(df["diagnosis"].value_counts())
+
+train_df, val_df = train_test_split(df, test_size=0.2, stratify=df["diagnosis"], random_state=42)
+train_df.to_csv(r"C:\VS Code\retinopathy_project\data\labels_train.csv", index=False, header=False)
+val_df.to_csv(r"C:\VS Code\retinopathy_project\data\labels_val.csv", index=False, header=False)
+print("Split complete. Files saved as labels_train.csv and labels_val.csv")
