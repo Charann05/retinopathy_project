bb5a461bdff71b0e81ea2ff103802d7cd460956f
Charann05 <charannpoojary@gmail.com>
2025-10-03 10:42:31 +0530

    Debugged data.py

--- src/data.py
+++ src/data.py
@@ -4,9 +4,18 @@ import pandas as pd
 from torch.utils.data import Dataset
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+import matplotlib.pyplot as plt
+
+#jk = None
 
 def read_image(path):
     img = cv2.imread(path)[:,:,::-1]
+
+    #plt.imshow(img)
+    #plt.show()
+    #global jk
+    #jk = img
+
     return img
 
 def basic_preprocess(img, size=512):
@@ -30,12 +39,11 @@ def get_transforms(train=False, size=512):
         return A.Compose([A.Resize(size,size), A.Normalize(), ToTensorV2()])
 
 class FundusDataset(Dataset):
-    #csv_file = r"C:\VS Code\retinopathy_project\data\labels.csv"
-    #images_dir = r"C:\VS Code\retinopathy_project\data\images"
     def __init__(self, csv_file, images_dir, transforms):
         csv_file = r"C:\VS Code\retinopathy_project\data\labels.csv"
         images_dir = r"C:\VS Code\retinopathy_project\data\images"
-        self.df = pd.read_csv(csv_file)
+
+        self.df = pd.read_csv(csv_file , names = ['image','label'])
         self.images_dir = images_dir
         self.transforms = transforms
 
@@ -43,24 +51,31 @@ class FundusDataset(Dataset):
         return len(self.df)
     def __getitem__(self, idx):
         row = self.df.iloc[idx]
-        path = os.path.join(self.images_dir, row['id_code'])
+        pat = os.path.join(self.images_dir, row['image'])
+        path = (f"{pat}.png")
+
+        #print("Reading:", path)
         img = read_image(path)
+
+        #if img is None:
+            #raise FileNotFoundError(f"Image not found at {path}")
+
         img = basic_preprocess(img)
         img = self.transforms(image=img)['image']
         label = int(row['label'])
         return img, label
-    
-csv_file = r"C:\VS Code\retinopathy_project\data\labels.csv"
-images_dir = r"C:\VS Code\retinopathy_project\data\images"    
+      
+
 #Code below is only for testing
 if __name__ == "__main__":
     transforms = get_transforms(train=False)
     dataset = FundusDataset("csv_file", "images_dir", transforms)
     print(f"Number of samples: {len(dataset)}")
-    img, label = dataset[0]
+    img, label = dataset[1]
     print(f"Image shape: {img.shape}, Label: {label}")
-
-
-#Errors to be solved
-#If solving do it in a seperate file
-        
+    
+    #if jk is None:
+        #print("Can't print")
+    #else:
+        #plt.imshow(jk)
+        #plt.show()
