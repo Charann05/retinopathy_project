f6e73e210df2da394767f056608d978537c1af07
Charann05 <charannpoojary@gmail.com>
2025-10-29 19:10:57 +0530

    Updated model.py

--- src/model.py
+++ src/model.py
@@ -1,6 +1,6 @@
 import timm
 import torch.nn as nn
 
-def create_model(n_classes=5, model_name='tf_efficientnet_b0_ns', pretrained=True):
+def create_model(n_classes=5, model_name='tf_efficientnet_b0.ns_jft_in1k', pretrained=True):
     model = timm.create_model(model_name, pretrained=pretrained, num_classes=n_classes)
     return model
