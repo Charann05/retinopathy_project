b2bfc46ace8575c052d41f7b810dce6103f0cebd
Charann05 <charannpoojary@gmail.com>
2025-10-29 19:14:09 +0530

    Updated infer.py

--- src/infer.py
+++ src/infer.py
@@ -3,7 +3,7 @@ from src.model import create_model
 from src.data import basic_preprocess, get_transforms
 
 model = create_model(n_classes=5)
-model.load_state_dict(torch.load('best_model.pth', map_location='cpu'))
+model.load_state_dict(torch.load('best_model.pth', map_location='cpu', weights_only=True))
 model.eval()
 
 path = input("Enter image path: ").strip()
@@ -19,5 +19,8 @@ tensor = transform(image=img)['image'].unsqueeze(0)
 with torch.no_grad():
     out = model(tensor)
     pred = out.argmax(dim=1).item()
+
 print("Predicted grade:", pred)
-plt.imshow(img); plt.title(f"Pred: {pred}"); plt.axis('off')
+plt.imshow(img); 
+plt.title(f"Pred: {pred}"); 
+plt.axis('off')
