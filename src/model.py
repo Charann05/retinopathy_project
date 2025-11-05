49353474f19eddb56f16195be30f6e4e7c50f704
dhyankj <dhyankj@gmail.com>
2025-11-05 20:47:19 +0530

    Updated model.py

--- src/model.py
+++ src/model.py
@@ -3,12 +3,6 @@
 import timm
 import torch.nn as nn
 
-def create_model(num_classes=5):
-    # Load the same architecture you trained with
-    model = timm.create_model('tf_efficientnet_b0.ns_jft_in1k', pretrained=False)
-    
-    # Change the final layer to match your number of classes
-    model.classifier = nn.Linear(model.classifier.in_features, num_classes)
-    
-
+def create_model(n_classes=5, model_name='tf_efficientnet_b0.ns_jft_in1k', pretrained=True):
+    model = timm.create_model(model_name, pretrained=pretrained, num_classes=n_classes)
     return model
